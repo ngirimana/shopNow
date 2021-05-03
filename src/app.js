8dab1f9e339a3f20f4fff61377e03c1df3021654
ngirimana <chadrackngirimana@gmail.com>
2021-05-03 08:46:40 +0200

    connect frontend with backend

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+import path from 'path';
 import bodyParser from 'body-parser';
 import cloudinary from 'cloudinary';
 import dotenv from 'dotenv';
@@ -13,6 +13,8 @@ import payment from './routes/payment';
 dotenv.config();
 const app = express();
 
+// Setting up config file
+if (process.env.NODE_ENV !== 'PRODUCTION') dotenv.config({ path: '../.env' });
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,13 +34,10 @@ app.use('/api/v1', products);
 app.use('/api/v1/auth', auth);
 app.use('/api/v1', order);
 app.use('/api/v1', payment);
-app.use("/", (req, res) => {
-	res.status(200).send({
-		status: 200,
-		message: "Welcome To ShopNow",
-	});
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
 });
-
 
 
 export default app;
