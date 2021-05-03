cae49b3b0d18ddc1b7293a97a762d481daf7da4d
ngirimana <chadrackngirimana@gmail.com>
2021-05-03 09:34:28 +0200

    using try catch to new product

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -18,35 +18,39 @@ class ProductController {
 	 * @returns {object} of created product data
 	 */
 	static async newProduct(req, res) {
-		   let images = []
-		if (typeof req.body.images === 'string') {
-			images.push(req.body.images)
-		} else {
-			images = req.body.images
-		}
+		try {
+			let images = [];
+			if (typeof req.body.images === 'string') {
+				images.push(req.body.images);
+			} else {
+				images = req.body.images;
+			}
 
-		const imagesLinks = [];
+			const imagesLinks = [];
 
-		for (let i = 0; i < images.length; i++) {
-			const result = await cloudinary.v2.uploader.upload(images[i], {
-				folder: 'products'
+			for (let i = 0; i < images.length; i++) {
+				const result = await cloudinary.v2.uploader.upload(images[i], {
+					folder: 'products',
+				});
+
+				imagesLinks.push({
+					public_id: result.public_id,
+					url: result.secure_url,
+				});
+			}
+
+			req.body.images = imagesLinks;
+			req.body.user = req.user.id;
+
+			const product = await Product.create(req.body);
+
+			res.status(201).json({
+				success: true,
+				product,
 			});
-
-			imagesLinks.push({
-				public_id: result.public_id,
-				url: result.secure_url
-			})
+		} catch (err) {
+			return errorResponse(res, 500, err.message);
 		}
-
-		req.body.images = imagesLinks
-		req.body.user = req.user.id;
-
-		const product = await Product.create(req.body);
-
-		res.status(201).json({
-			success: true,
-			product
-		})
 
 	}
 
